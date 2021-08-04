9d1bf4ded02c0c0919ea89343618d36f387e7eff
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-08-04 18:35:50 -0500

    Adds training procedure based on phastic-policy-gradient

--- mrl/train_oracles.py
+++ mrl/train_oracles.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
 import fire
+import gym3
 import numpy as np
+from mpi4py import MPI
 from numpy.random import Generator
+from phasic_policy_gradient.train import train_fn
 
 from mrl.envs import Miner
 
 
 def make_reward(path: Path, rng: Generator) -> np.ndarray:
-    reward = rng.multivariate_normal(mean=0, cov=1, size=(Miner.N_FEATURES,))
+    reward = rng.standard_normal(size=(Miner.N_FEATURES,))
     reward = reward / np.linalg.norm(reward)
     np.save(path / "reward.npy", reward)
     return reward
@@ -21,10 +24,17 @@ def load_reward(path: Path, rng: Generator, overwrite: bool = False) -> np.ndarr
 
 
 def train(path: Path, overwrite: bool = False, seed: int = 0, n_parallel_envs: int = 1) -> None:
+    path = Path(path)
+
     rng = np.random.default_rng(seed)
     reward = load_reward(path, rng, overwrite)
 
     env = Miner(reward_weights=reward, num=n_parallel_envs, rand_seed=seed)
+    env = gym3.ExtractDictObWrapper(env, "rgb")
+
+    comm = MPI.COMM_WORLD
+
+    train_fn(arch="detach", n_epoch_vf=6, log_dir=path, comm=comm, venv=env)
 
 
 if __name__ == "__main__":
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="multimodal-reward-learning", packages=find_packages(), version="0.0.1",
+)
