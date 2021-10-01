Fixes bug in miner min distance computation.

mrl/envs/miner.py
@@ -188,7 +188,7 @@ class Miner(ProcgenGym3Env):
         diamonds = cast(np.ndarray, np.logical_or(state.grid == 2, state.grid == 4))
         # TODO(joschnei): Instead of indexing into this, try creating it on the fly, might be faster
         dists = get_dist_array(agent_x, agent_y, width, height)
-        diamond_dists = np.ma.array(dists, mask=diamonds)
+        diamond_dists = np.ma.array(dists, mask=np.logical_not(diamonds))
         pos_closest_diamond = cast(
             Tuple[int, int], np.unravel_index(diamond_dists.argmin(), diamond_dists.shape)
         )