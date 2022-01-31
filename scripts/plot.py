2297ecf7f2750f7d4ff3714e3a88d9a128804eef
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-01-31 11:00:54 -0800

    Adds post hoc plot for flip_probs

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 import fire  # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns  # type: ignore
 from mrl.inference.posterior import Results
 
@@ -19,5 +21,17 @@ def prob_aligned(rootdir: Path, max_comparisons: int = 100) -> None:
     ).savefig(rootdir / f"prob_aligned.first_{max_comparisons}.png")
 
 
+def flip_prob(flip_probs_path: Path, nbins: int = 100) -> None:
+    flip_probs_path = Path(flip_probs_path)
+    flip_probs = np.load(flip_probs_path)
+
+    temp = float(flip_probs_path.parts[-2])
+
+    plt.hist(flip_probs, bins=nbins)
+    plt.xlim((0, 1))
+    plt.title(f"Flip probabilities for temp={temp:0.5f}")
+    plt.savefig(flip_probs_path.with_suffix(".png"))
+
+
 if __name__ == "__main__":
-    fire.Fire({"prob-aligned": prob_aligned})
+    fire.Fire({"prob-aligned": prob_aligned, "flip-prob": flip_prob})
