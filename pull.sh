bda044a01576ab7aab5cb83d24f2729ea830d52f
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-10-14 15:42:07 -0400

    Updates pull script to include all the new folders.

--- pull.sh
+++ pull.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-git pull
 cd ../procgen
+git pull
+cd ../linear-procgen
+git pull
+cd ../procgen-experiment
+git pull
+cd ../experiment-server
 git pull
 cd ../phasic-policy-gradient
 git pull
 cd ../multimodal-reward-learning
+git pull
