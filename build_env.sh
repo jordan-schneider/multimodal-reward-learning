bdf322105edfafc3c7a9ac47ebd5487d7e033443
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-11-22 19:27:07 -0800

    Makes editable installation strict to make pylance happy. Fixes issue where subpackages weren't listed in build, switched to setuptools package finder.

--- build_env.sh
+++ build_env.sh
@@ -4,4 +4,4 @@ then
 	mamba env create -f environment.yml
 fi
 mamba run -n mrl pip install --force-reinstall git+https://github.com/jordan-schneider/linear-procgen.git
-mamba run -n mrl pip install -e .
+mamba run -n mrl pip install -e . --config-settings editable_mode=strict
